a79d69361b3c35f490214865e9135769dc6feb6b
Dax Crowley <166465964+aquakid@users.noreply.github.com>
2025-03-03 10:12:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 //javascript
 /*Basically we just need to make a few houses up on hill. The hill because we are countering sea level rising, the houses because we are making a residential development, try to make it look like San Francisco too*/
+
+//Dax   ok sounds good. 
